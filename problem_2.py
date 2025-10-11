e5bbd114627013702cd47ff03ec0c75bac58af4e
visal-512 <visal.512@icloud.com>
2025-10-11 15:29:25 +0100

    Added comments

--- problem_2.py
+++ problem_2.py
@@ -1,12 +1,12 @@
 def sum_of_even_fib(limit):
-    x, y, z = 1, 1, 0
-    sum = 0
-    while z < limit:
+    x, y, z = 1, 1, 0 # assign values for the initial sequence values
+    result = 0
+    while z < limit: # terminates loop once final number in sequence is less than the limit
         z = (x+y)         
         if z%2 == 0:
-            sum += z
+            result += z # only adds to the result if the value is even
         x = y
         y = z
-    return sum
+    return result
 
 print(sum_of_even_fib(4000000))
