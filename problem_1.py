2850b8cb54ed921d1f3a8db91dab9c0600739d17
visal-512 <visal.512@icloud.com>
2025-10-11 15:29:07 +0100

    Added comments

--- problem_1.py
+++ problem_1.py
@@ -2,10 +2,10 @@ def multiples_of_3_5(limit):
     total = 0
     for i in range(1, limit):
         if i % 3 == 0:
-            total += i
+            total += i # add the value to the total if it is divisible by 3
         elif i % 5 == 0:
-            total += i
+            total += i # add the value to the total if it is divible by 5 and not 3
         else: continue
     print(total)
 
-multiples_of_3_5(1000)
+multiples_of_3_5(1000) # print the total value found in the func
